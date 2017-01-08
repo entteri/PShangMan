e9fd3fc7b22f59d09e723e44807c4118d8b2a60e
Daniel Mononen <mononen.daniel@gmail.com>
2017-01-08 09:22:25 +0200

    first commit

---
+++ class-hirsipuu.psm1
@@ -0,0 +1,7 @@
+﻿Class Hirsipuu {
+    $guessedLetters = (New-Object System.Collections.ArrayList)
+    [int]$guessesLeft = 5
+    [string[]]$words = @("smothie")
+    [string]$correctWord = (Get-Random -inputObject $this.words)
+    [string]$guessedWord = ""
+}
